c94ba0221a7649989b29f5a4c23a3e48688e0dd1
thiagorafaelferreira <thiagorafaelferreira@gmail.com>
2016-11-29 01:10:38 -0200

    thread

---
+++ src/threads/Programa.java
@@ -0,0 +1,23 @@
+package threads;
+
+public class Programa implements Runnable {
+
+	private int id;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < 10000; i++) {
+            System.out.println("Programa " + id + " valor: " + i);
+        }
+	}
+
+}
